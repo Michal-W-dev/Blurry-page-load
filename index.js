7644280def126d3512b0d9cd09ff1654b1ff280e
Michal-W-dev <70209633+Michal-W-dev@users.noreply.github.com>
2021-04-28 13:56:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Blurry loading ------------------------------------
 const bg = document.querySelector('.page-bg')
 const loader = document.querySelector('.loader')
 const searches = document.querySelectorAll('.search')
@@ -35,3 +36,53 @@ const id = setInterval(() => {
         }, 250)
     }
 }, 10)
+
+// Progress bar ----------------------------------------
+const progress = document.querySelector('.progress')
+const prev = document.querySelector('.btn-prev')
+const next = document.querySelector('.btn-next')
+const circles = document.querySelectorAll('.circle')
+const actives = document.querySelectorAll('.active')
+
+let currentActive = 1;
+const circlesLength = circles.length;
+
+
+function disableBtns() {
+    if (currentActive === 1) {
+        prev.disabled = true;
+    } else if (currentActive === circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false
+    }
+}
+
+function update() {
+    circles.forEach((circle, i) => {
+        if (currentActive > i) {
+            circle.classList.add('active');
+        } else circle.classList.remove('active');
+    })
+    let width = ((currentActive - 1) / (circlesLength - 1)) * 99
+    progress.style.width = width + '%'
+    disableBtns()
+}
+
+next.addEventListener('click', () => {
+    currentActive++
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if (currentActive <= 1) {
+        currentActive = 1;
+    }
+    update()
+})
